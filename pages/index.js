d17f8013336a250789f4869a0b067328dd806699
bradvader <vader.brad@gmail.com>
2022-05-12 15:39:29 -0400

    Footer - 75% done

--- pages/index.js
+++ pages/index.js
@@ -78,8 +78,71 @@ export default function Home() {
     </div>
 
     <div className="" id="bv-footer-block">
-
-      
+      <div className="footer-inner">
+        <div className="foot-line"></div>
+      <div className="foot-logo">
+        <div className="logo-word"></div>
+        <div className="logo-brand"></div>
+      </div>
+      <div className="frame-140">
+  <div className="frame-40">
+  <div className="footer-heading">Careers</div>
+  <div className="foot-item">Careers Overview</div>
+  <div className="foot-item">Open Positions</div>
+ 
+  </div>
+  <div className="frame-39">
+  <div className="footer-heading">Company</div>
+  <div className="foot-item">Company Overview</div>
+  <div className="foot-item">Awards</div>
+  <div className="foot-item">In the News</div>
+  <div className="foot-item">Upcoming Events</div>
+  <div className="foot-item">Partnerships</div>
+  <div className="foot-item">Contact</div>
+  </div>
+  
+</div>
+<div className="frame-37">
+  <div className="footer-heading">Platform</div>
+  <div className="foot-item">Platform Overview</div>
+  <div className="foot-item">Order and Execution Management</div>
+  <div className="foot-item">Portfolio Management</div>
+  <div className="foot-item">Accounting</div>
+  <div className="foot-item">Analytics</div>
+  <div className="foot-item">Connectivity, Data, APIs</div>
+  <div className="foot-item">Account Managment and Client Support</div>
+</div>
+<div className="frame-42">
+  <div className="footer-heading">Solutions</div>
+  <div className="foot-item">Solutions Overview</div>
+  <div className="foot-item">New Fund Launches</div>
+  <div className="foot-item">Funds Outgrowing Their Systems</div>
+  <div className="foot-item">Hedge Funds</div>
+  <div className="foot-item">Institutional Asset Managers</div>
+  <div className="foot-item">Family Offices</div>
+  <div className="foot-item">Portfolio Managers</div>
+  <div className="foot-item">Traders</div>
+  <div className="foot-item">CFOs and Accountants</div>
+  <div className="foot-item">COOs and Operational Leadership</div>
+  <div className="foot-item">Compliance and Risk</div>
+  <div className="foot-item">CTOs and IT Systems</div>
+</div>
+<div className="frame-139">
+  <div className="frame-38">
+  <div className="footer-heading">Strategic Services</div>
+  <div className="foot-item">Strategic Services Overview</div>
+  <div className="foot-item">Middle Office Managed Services</div>
+  <div className="foot-item">Back Office Managed Services</div>
+  </div>
+  <div className="frame-41">
+  <div className="footer-heading">Insights</div>
+  <div className="foot-item">Latest Insights</div>
+  <div className="foot-item">Fund Launches</div>
+  <div className="foot-item">Scaling operations</div>
+  </div>
+  
+</div>
+</div>
     </div>
 
 
