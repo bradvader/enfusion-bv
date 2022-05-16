e580cac459e4647f56715f0f62ea9e26caa58eee
bradvader <vader.brad@gmail.com>
2022-05-16 08:28:01 -0400

    Monday May 16 Updates - Nearly done 4

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,6 @@ import Head from 'next/head';
 
 import Layout from "../layouts/Layout";
 import NavBar from "../components/NavBar";
-import Hero from "../components/Hero";
-import HeroCarousel from "../components/HeroCarousel";
-import HeroSlider from "../components/HeroSlider";
-import CTA1 from "../components/CTA";
 // import NavBar from "../components/Navbar";
 
 import Solution from '../components/Solution';
@@ -21,7 +17,6 @@ import Modal from "../components/Modal"
 //import NumberModule from "../components/NumberModule";
 import Slidey from "../components/Slidey";
 
-import CTAModule from "../components/CTAModule";
 
 
 
@@ -33,25 +28,6 @@ export default function Home() {
       
      <Slidey />
       <Why />
-      
-
-        {/* <div id="hero-img-1">
-        <div id="rectangle-5"></div>
-          <div id="hero-1-inner">
-
-          </div>
-        </div>
-        <div id="rectangle-3"></div>
-        <div id="bv-solution-block">
-
-        </div>
-
-
-        <div id="bv-headder"></div>
-
-        <div id="devices"></div> */}
-
-        {/* <Benefits /> */}
         <Solution />
 
         <Personas />
@@ -59,16 +35,8 @@ export default function Home() {
         {/* <Car /> */}
         <Statistics />
         <Awards />
-
-
-
-
-
         <CTA2 />
-
        <Footer />
-        {/* <div id="subtract"></div> */}
-
     </Layout>
   );
 }
